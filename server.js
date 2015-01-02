abb9dfcd3ae752313b903827caf81b35b56ec7a8
Liam Doherty <lfdoherty@gmail.com>
2015-01-01 16:42:37 -0800

    properly listening server

--- server.js
+++ server.js
@@ -17,31 +17,59 @@ var port = 8099
 var server = jot.createServer(port);
 
 server.on('connection', function (c){
-	var openPipelines = {}
+	var openPipeline
+	var openPipelineKey
+	var configByKey = {}
+	var openPipelineIdMap = {}
 	c.on('data', function(data){
 		if(data.type === 'create-pipeline'){
-			createPipeline(data.annotators, function(err, core){
-				if(err){
-					socket.write({type: 'error', errcode: 'pipeline-creation-error', req: data.req, err: err})
-					return
-				}
-				openPipelines[data.req] = core
+			var key = JSON.stringify(data.annotators)
+			openPipelineIdMap[data.req] = key
+			if(openPipelineKey === key){
 				socket.write({type: 'pipeline-created', req: data.req, id: data.req})
-			})
+			}else{
+				openPipelineKey = key
+				configByKey[key] = data.annotators
+				openPipeline = undefined
+				createPipeline(data.annotators, function(err, core){
+					if(err){
+						socket.write({type: 'error', errcode: 'pipeline-creation-error', req: data.req, err: err})
+						return
+					}
+					openPipeline = core//s[data.req] = core
+					socket.write({type: 'pipeline-created', req: data.req, id: data.req})
+				})
+			}
 		}else if(data.type === 'process'){
+			if(data.pipeline !== openPipelineKey){
+				//reload desired pipeline
+				createPipeline(configByKey[openPipelineIdMap[data.pipeline]], function(err, core){
+					if(err){
+						socket.write({type: 'error', errcode: 'pipeline-re-creation-error', req: data.req, err: err})
+					}else{
+						openPipeline = core
+						openPipelineKey = openPipelineIdMap[data.pipeline]
+						doProcess(core)
+					}
+				})
+				return
+			}
 			var pipeline = openPipelines[data.pipeline]
 			if(!pipeline){
 				socket.write({type: 'error', errcode: 'unknown-pipeline', err: 'Uknown pipeline: ' + data.pipeline})
 				return
 			}
-			pipeline.process(data.text, function(err, result){
-				if(err){
-					socket.write({type: 'error', errcode: 'process-failed', err: err})
-					return
-				}
-				socket.write({type: 'process-result', req: data.req, result: result})
-			})
-		}else if(data.type === 'destroy-pipeline'){
+			doProcess(pipeline)
+			function doProcess(pipeline){
+				pipeline.process(data.text, function(err, result){
+					if(err){
+						socket.write({type: 'error', errcode: 'process-failed', err: err})
+						return
+					}
+					socket.write({type: 'process-result', req: data.req, result: result})
+				})
+			}
+		}/*else if(data.type === 'destroy-pipeline'){
 			var pipeline = openPipelines[data.pipeline]
 			if(!pipeline){
 				socket.write({type: 'error', errcode: 'unknown-pipeline', err: 'Uknown pipeline: ' + data.pipeline})
@@ -49,9 +77,12 @@ server.on('connection', function (c){
 			}
 			delete openPipelines[data.pipeline]
 			socket.write({type: 'pipeline-destroyed', req: data.req, pipeline: data.pipeline})
-		}else{
+		}*/else{
 			console.log('unknown request type: ' + data.type)
 		}
 	});
 });
+
+server.listen(port)
+
 
